a76fca896419d970119f7b113d76b9cf407ce1b0
leasamson65-oss <leasamson65@gmail.com>
2026-02-06 10:12:52 +0800

    Add files via upload

---
+++ the__init__method.py
@@ -0,0 +1,34 @@
+class Person:
+    def __init__(self, name, age, height_in_cm):
+        self.name = name
+        self.age = age
+        self.height_in_cm = height_in_cm
+    def speak(self):
+                print(f"Hello, my name is {self.name} and I am {self.age} years old and.")
+
+                adam = Person("Adam", 47, 193)
+                lovelance = Person("Love Lance", 24, 178)
+                lucer = Person("Luce", 13, 154)
+                adam.speak()
+        Hello! My name is Adam and I am 47 years old.
+           lovelance.speak()
+        Hello! My name is LoveLance and I am 24 years old.
+           lucer.speak()
+        Hello! My name is Luce and I am 13 years old.
+
+                      
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                )
