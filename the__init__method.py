e0d2692138d9cc59e0675ba5cd46c318c8421b94
leasamson65-oss <leasamson65@gmail.com>
2026-02-06 10:17:20 +0800

    Refactor Person class and fix speak method calls

--- the__init__method.py
+++ the__init__method.py
@@ -6,17 +6,16 @@ class Person:
     def speak(self):
                 print(f"Hello, my name is {self.name} and I am {self.age} years old and.")
 
-                adam = Person("Adam", 47, 193)
-                lovelance = Person("Love Lance", 24, 178)
-                lucer = Person("Luce", 13, 154)
-                adam.speak()
-        Hello! My name is Adam and I am 47 years old.
-           lovelance.speak()
-        Hello! My name is LoveLance and I am 24 years old.
-           lucer.speak()
-        Hello! My name is Luce and I am 13 years old.
+adam = Person("Adam", 47, 193)
+lovelace = Person("Lovelance", 24, 178)
+lucer = Person("Luce", 13, 154)
 
-                      
+adam.speak()
+("Hello! My name is Adam and I am 47 years old.")
+lovelace.speak()
+("Hello! My name is Lovelace and I am 24 years old.")
+lucer.speak()
+("Hello! My name is Luce and I am 13 years old.")
                 
                 
                 
@@ -31,4 +30,5 @@ class Person:
                 
                 
                 
+
                 )
